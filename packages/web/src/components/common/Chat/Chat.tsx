3371526458d30155f99306aca1ff653aec3ff7e3
Michael Carolin <mpcarolin.dev@gmail.com>
2025-08-27 17:06:20 -0700

    More style tweaks

--- packages/web/src/components/common/Chat/Chat.tsx
+++ packages/web/src/components/common/Chat/Chat.tsx
@@ -74,7 +74,7 @@ const SendMessageIcon = () => {
             width="32"
             height="32"
             viewBox="0 0 24 24"
-            fill="none"
+            fill="currentColor"
             xmlns="http://www.w3.org/2000/svg"
         >
             <path
